73fd01b4a27b7ac3978a3717ec5fc899a2b2d23e
Lam Manh Hung <hunglmhe171165@fpt.edu.vn>
2023-02-22 22:10:52 +0700

    add search class and calendar

--- src/screens/CalendarScreens.tsx
+++ src/screens/CalendarScreens.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
-
-const CalendarScreens = () => {
+import React,{useState} from "react";
+import { View, Text, Modal, ImageBackground } from "react-native";
+import { Calendar } from "react-native-calendars";
+import MenuButton from "../components/MenuButton";
+const CalendarScreens = ({ navigation }: any) => {
+  const [currentDate,setCurrntDate] = useState();
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/background.png")}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <View style={{}}>
+        <MenuButton onPress={() => navigation.openDrawer()} />
+      </View>
+      <Calendar
+        style={{ margin: 30, borderRadius: 10, elevation: 100 }}
+        onDayPress={(day : any) => {
+          setCurrntDate(day.dateString);
+          console.log(currentDate);
+          
+        }}
+        markedDates = {
+          {
+            currentDate : { selected : true}
+          }
+        }
+      />
+    </ImageBackground>
   );
 };
 
--- src/screens/StudentRole/HomeScreens.tsx
+++ src/screens/StudentRole/HomeScreens.tsx
@@ -1,40 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   View,
   TouchableOpacity,
   ImageBackground,
   SafeAreaView,
   StyleSheet,
+  Touchable,
+  TouchableWithoutFeedback,                                             
 } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import ListItem from "../../components/ListItem";
 import MenuButton from "../../components/MenuButton";
 import SearchBar from "../../components/SearchBar";
 
 const HomeScreens = ({ navigation }: any) => {
+  const [listClass,setListClass] = useState(
+    [
+      "AI 1705", "SE 1746", "IT 1650", "IA 1578", "AI 1746", "SE 1740", "IS 1500", "SE 1733","IA 1544", "SE 1333", 
+    ]
+  );
+   const [listClassSearch, setListClassSearch] = useState(listClass);
+   const handleSearch = (text : string) => {
+        console.log(text);
+        
+        setListClassSearch(listClass.filter((e) => {return e.includes(text.toUpperCase())})) 
+   }
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      {/* <TouchableWithoutFeedback> */}
       <ImageBackground
         source={require("../../../assets/background.jpg")}
         resizeMode="cover"
         style={styles.ImageBackground}
-      >
+        >
         <View style={styles.MenuTouchableContainer}>
           <MenuButton onPress={() => navigation.openDrawer()} />
-          <SearchBar onTextChange={() => {}} />
+          <SearchBar onTextChange= {handleSearch} />
         </View>
-        <View style={styles.ListItemContainer}>
-          <TouchableOpacity
-            style={styles.Touchable}
-            onPress={() => navigation.navigate("Tabs")}
-          >
-            <ListItem>AI 1705</ListItem>
-          </TouchableOpacity>
-          <ListItem>SE 1740</ListItem>
-          <ListItem>IT 1650</ListItem>
-          <ListItem>IA 1578</ListItem>
-          <ListItem>AI 1706</ListItem>
-        </View>
+        <ScrollView >
+           {
+             listClassSearch.map((e,index) => {
+                return <TouchableOpacity  key = {index} style={styles.Touchable}
+                onPress={() => navigation.navigate("Tabs")}>
+                   <ListItem key = {index} children = {e}/>
+                </TouchableOpacity>
+             })
+           }
+        </ScrollView>
       </ImageBackground>
+      {/* </TouchableWithoutFeedback> */}
     </SafeAreaView>
   );
 };
