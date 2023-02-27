4eece67f3096ef62967831ce9ca420c0f4f207bc
mmotkim <mmotkim2@gmail.com>
2023-02-27 20:57:16 +0700

    restyled

--- src/screens/TeacherRole/TakeAttendanceScreens.tsx
+++ src/screens/TeacherRole/TakeAttendanceScreens.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import ClassInfoHeader from "../../components/ClassInfoHeader";
 import { List } from "react-native-paper";
 import StatusBarLight from "../../components/StatusBarLight";
+import { Feather } from '@expo/vector-icons';
 
 const TakeAttendanceScreens = () => {
   return (
@@ -10,53 +11,52 @@ const TakeAttendanceScreens = () => {
       <ClassInfoHeader
         className="A5"
         courseName="MAS"
-        title="Material"
+        title="Take Attendance"
         navigation={undefined}
       />
 
       <Text
         style={{
           textAlign: "center",
-          paddingTop: 20,
+          padding: 20,
           fontSize: 18,
-          fontFamily: "brandon-reg",
+          fontFamily: "brandon",
         }}
       >
         Press on your lesson to specify attendance taking
       </Text>
 
       <View>
-        <List.AccordionGroup>
-          <List.Accordion //controlled
-            title="Today(2)"
-            expanded={undefined}
-            onPress={undefined}
-          >
-            <List.Item title="Lesson 4" />
-            <List.Item title="Lesson 5" />
-          </List.Accordion>
+        <List.Accordion //controlled
+          title="Today(2)"
+          expanded={undefined}
+          onPress={undefined}
+        >
+          <List.Item title="Lesson 4" description="0/1" right={() => <Feather name="user-check" size={24} color="black" />}/>
+          <List.Item title="Lesson 5" description="0/1" right={() => <Feather name="user-check" size={24} color="black" />}/>
+        </List.Accordion>
 
-          <List.Accordion //controlled
-            title="Finished(3)"
-            expanded={undefined}
-            onPress={undefined}
-          >
-            <List.Item title="Lesson 1" />
-            <List.Item title="Lesson 2" />
-            <List.Item title="Lesson 3" />
-          </List.Accordion>
+        <List.Accordion //controlled
+          title="Finished(3)"
+          expanded={undefined}
+          onPress={undefined}
+        >
+          <List.Item title="Lesson 1" description="1/1" right={() => <Feather name="user-check" size={24} color="black" />}/>
+          <List.Item title="Lesson 2" description="1/1" right={() => <Feather name="user-check" size={24} color="black" />}/>
+          <List.Item title="Lesson 3" description="1/1" right={() => <Feather name="user-check" size={24} color="black" />}/>
+        </List.Accordion>
 
-          <List.Accordion //controlled
-            title="Future(4)"
-            expanded={undefined}
-            onPress={undefined}
-          >
-            <List.Item title="Lesson 6" />
-            <List.Item title="Lesson 7" />
-            <List.Item title="Lesson 8" />
-            <List.Item title="Lesson 9" />
-          </List.Accordion>
-        </List.AccordionGroup>
+        <List.Accordion //controlled
+          title="Future(4)"
+          expanded={undefined}
+          onPress={undefined}
+        >
+          <List.Item title="Lesson 6" description="0/1"/>
+          <List.Item title="Lesson 7" description="0/1"/>
+          <List.Item title="Lesson 8" description="0/1"/>
+          <List.Item title="Lesson 9" description="0/1"/>
+
+        </List.Accordion>
       </View>
     </View>
   );
