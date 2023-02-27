a05b12d1391e33a2195cc15d0a38c8a965d627a6
Lam Manh Hung <hunglmhe171165@fpt.edu.vn>
2023-02-27 12:34:00 +0700

    Update calendar

--- src/screens/CalendarScreens.tsx
+++ src/screens/CalendarScreens.tsx
@@ -1,32 +1,74 @@
-import React,{useState} from "react";
-import { View, Text, Modal, ImageBackground } from "react-native";
-import { Calendar } from "react-native-calendars";
+import React, {useState} from 'react';
+import {View, TouchableOpacity,Text, ImageBackground} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {Card, Avatar} from 'react-native-paper';
 import MenuButton from "../components/MenuButton";
-const CalendarScreens = ({ navigation }: any) => {
-  const [currentDate,setCurrntDate] = useState();
-  return (
-    <ImageBackground
-      source={require("../../assets/background.png")}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <View style={{}}>
-        <MenuButton onPress={() => navigation.openDrawer()} />
-      </View>
-      <Calendar
-        style={{ margin: 30, borderRadius: 10, elevation: 100 }}
-        onDayPress={(day : any) => {
-          setCurrntDate(day.dateString);
-          console.log(currentDate);
-          
-        }}
-        markedDates = {
-          {
-            currentDate : { selected : true}
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
+
+const Schedule: React.FC = ({navigation} : any) => {
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = 2;
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Lesson ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
           }
         }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 500);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height : 60
+              }}>
+              {/* <Typography>{item.name}</Typography> */}
+              <Text>{item.name}</Text>
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <ImageBackground source={require('../../assets/background.jpg')} style={{flex : 1}}>
+      <MenuButton onPress={() => navigation.openDrawer()}/>
+      <Agenda
+        items={items}
+        calendarStyle = {{width : '100%', margin : 0}}
+        style = {{ marginHorizontal : 10,elevation : 100, borderRadius : 10}}
+        loadItemsForMonth={loadItems}
+        selected={timeToString(Date.now())}
+        renderItem={renderItem}
       />
     </ImageBackground>
   );
 };
 
-export default CalendarScreens;
+export default Schedule;
