b4b9fa804e6ef273017b03741022e1923c46a800
mmotkim <mmotkim2@gmail.com>
2023-04-10 10:57:25 +0700

    a

--- src/components/StatusBarLight.tsx
+++ src/components/StatusBarLight.tsx
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Platform } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
 import Constants from "expo-constants";
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
+
+// StatusBar.setBarStyle('dark-content');
+
+// if (Platform.OS === 'android') {
+//   StatusBar.setTranslucent(true);
+//   StatusBar.setBackgroundColor('transparent');
+// }
+
 //https://coolors.co/palette/cdb4db-ffc8dd-ffafcc-bde0fe-a2d2ff
 //status bar khi dien thoai o che do light mode (chu khong phai text tren status bar la` mau` trang)
 class Statusbar extends Component {
+  //   render() {
+  //     return (
+  //       <View style={styles.StatusBar}>
+  //         <StatusBar translucent style="light" />
+  //       </View>
+  //     );
+  //   }
+  // }
   render() {
     return (
-      <View style={styles.StatusBar}>
-        <StatusBar translucent style="light" />
-      </View>
+      <SafeAreaView>
+        <StatusBar
+          animated={true}
+          backgroundColor="#61dafb"
+          barStyle={"default"}
+        />
+      </SafeAreaView>
     );
   }
 }
---
+++ src/screens/StudentRole/HomeScreens2.tsx
@@ -0,0 +1,25 @@
+import { View, StyleSheet, Text } from "react-native";
+
+export default function HomeScreens2(){
+
+    return(
+        <>
+            <View>
+                <Text>
+                Home screen with multiple functions into one.
+                </Text>
+            </View>
+            
+        </>
+    )
+
+};
+
+const styles = StyleSheet.create({
+    mainContainer:  {
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+
+    }
+});
